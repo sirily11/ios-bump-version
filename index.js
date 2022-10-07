93df6808ecfef2df46b0b6b143619e23ce486ed4
sirily11 <sirily1997@gmail.com>
2022-10-07 16:23:00 +0800

    feat: using new version system

--- index.js
+++ index.js
@@ -1,64 +1,42 @@
-const core = require('@actions/core')
-const exec = require('@actions/exec')
-const fs = require('fs')
-const semver = require("semver")
+const core = require("@actions/core");
+const exec = require("@actions/exec");
+const fs = require("fs");
+const semver = require("semver");
+const glob = require("glob");
+const { parse, build } = require("xcparse");
 
-async function execCommand(command, options = {}) {
-    const projectPath = core.getInput('project-path')
-    options.cwd = projectPath
-    return exec.exec(command, [], options)
+function updateVersion(projectFile, nextVersion, nextBuildNumber) {
+  // go through each key value pair in the project file
+  for (const key in projectFile.objects) {
+    const value = projectFile.objects[key];
+    const buildSettings = value?.buildSettings;
+    const marketingVer = buildSettings?.MARKETING_VERSION;
+    const projectVer = buildSettings?.CURRENT_PROJECT_VERSION;
+
+    if (marketingVer !== undefined) {
+      buildSettings.MARKETING_VERSION = nextVersion;
+    }
+
+    if (projectVer !== undefined) {
+      buildSettings.CURRENT_PROJECT_VERSION = nextBuildNumber;
+    }
+  }
 }
 
 async function run() {
-    let version = core.getInput('version')
-    const buildNumber = core.getInput('build-number')
-    const versionPath = core.getInput('version-path')
+  let version = core.getInput("version");
+  const buildNumber = core.getInput("build-number");
 
-    if (versionPath) {
-        const content = fs.readFileSync(versionPath, 'utf8')
-        version = content.trim()
-    }
+  core.info(`Setting Version: ${version}, Build Number: ${buildNumber}`);
 
-    let parsedVersion = semver.parse(version)
-    let newVersion = `${parsedVersion.major}.${parsedVersion.minor}.${parsedVersion.patch}`
+  const projFilePath = glob.sync("*.pbxproj")[0];
+  core.info(`Found Project File Path: ${projFilePath}`);
 
-    if (version) {
-        core.setOutput('version', newVersion)
-
-        const command = `agvtool new-marketing-version ${newVersion}`
-        console.log(command)
-        await execCommand(command).catch(error => {
-            core.setFailed(error.message)
-        })
-    } else {
-        await execCommand(`agvtool what-marketing-version -terse1`, {
-            listeners: { stdout: (data) => {
-                console.log(data)
-                core.setOutput('version', data.toString().trim())
-            }}
-        })
-    }
-
-    if (!buildNumber) {
-        const command = `agvtool next-version -all`
-        console.log(command)
-        await execCommand(command).catch(error => {
-            core.setFailed(error.message)
-        })
-    } else {
-        const command = `agvtool new-version -all ${buildNumber}`
-        console.log(command)
-        await execCommand(command).catch(error => {
-            core.setFailed(error.message)
-        })
-    }
-
-    await execCommand(`agvtool what-version -terse`, {
-        listeners: { stdout: (data) => {
-            console.log(data)
-            core.setOutput('build-number', data.toString().trim())
-        }}
-    })
+  const projFile = fs.readFileSync(projFilePath, "utf8");
+  const proj = parse(projFile);
+  updateVersion(proj, version, buildNumber);
+  const newProjFile = build(proj);
+  fs.writeFileSync(projFilePath, newProjFile);
 }
 
-run()
+run();
